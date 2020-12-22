356d80215b2ca501bf95f61d0d24db4ceaf0d1df
K.DH3 <30466064+331leo@users.noreply.github.com>
2020-12-23 01:10:05 +0900

    Create storeconfig_example.py

---
+++ storeconfig_example.py
@@ -0,0 +1,16 @@
+TossClientKey = "TOSS API KEY"
+TossSecretKey= "TOSS SECRET KEY"
+
+coolsms_api_key = "COOLSMS API KEY"
+coolsms_api_secret = "COOLSMS API SECRET"
+coolsms_api_number = "COOLSMS PHONENUM"
+
+firebase_web_cert={
+"apiKey": "FIREBASE WEB CERTS",
+"authDomain": "FIREBASE WEB CERTS",
+"projectId": "FIREBASE WEB CERTS",
+"storageBucket": "FIREBASE WEB CERTS",
+"messagingSenderId": "FIREBASE WEB CERTS",
+"appId": "FIREBASE WEB CERTS",
+"measurementId": "FIREBASE WEB CERTS"
+}
